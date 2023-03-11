5a48eb448012fa0ce2532fbbbcdb3f197421d817
b0wter <b0wter@gmx.de>
2023-03-12 00:40:11 +0100

    Began recreating the HexmapScene

--- src/xelmish/Screens/OverworldScreen.fs
+++ src/xelmish/Screens/OverworldScreen.fs
@@ -1,17 +1,54 @@
 namespace Friends.Xelmish.Screens
 
+open System
+open System.IO
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open MonoGame.Extended
+open TiledCS
+
 module OverworldScreen =
     open Xelmish.Viewables
     open Xelmish.Model
     
-    type Model = {
-        Counter: int
+    /// Used in the transformation matrix to render the tile SpriteBatch
+    let private ScaleFactor = 1.0f // XNA uses single precision floats
+    
+    let private DefaultTransformationMatrix = Matrix.CreateScale (ScaleFactor, ScaleFactor, 1.0f)
+    
+    let private Sqrt3 = MathF.Sqrt 3.0f
+    
+    type private TileSetKey = {
+        TileSetId: int
+        Offset: int
     }
     
-    let init () =
+    type Model = {
+        TiledMap: TiledMap
+        /// <summary>
+        /// Size of the tiles in pixels. It's the number of pixel from the center to the outer circle of the hexagon.
+        /// This is equivalent to the distance from the center to one of the "spikes"
+        /// </summary>
+        /// <remarks>
+        /// See https://www.redblobgames.com/grids/hexagons/#basics for the definition of `Size`.
+        /// </remarks>
+        TileSize: int
+        TileSets: Map<int, TiledTileset>
+        TileSetTextures: Map<int, Texture2D>
+        /// <summary>
+        /// Maps each id used in the TileMap to tileset id and an offset. The offset is the position of the tile in the
+        /// tileset
+        /// </summary>
+        TileSetTileIds: Map<int, TileSetKey>
+        Camera: OrthographicCamera
+    }
+    
+    let init (tiledMapName: string) =
+        let tiledMap = Path.Combine ()
         {
             Counter = 0
         }
+        
         
     type Message =
         | CameraUp
